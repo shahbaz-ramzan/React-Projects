e1d4b1881d761444970a8ade30f4a5a0a810043c
Shahbaz-ramzan-01 <mshahbazramzan952@gmail.com>
2024-03-17 21:18:23 +0500

    latest code

--- form-validations/src/components/first-form.jsx
+++ form-validations/src/components/first-form.jsx
@@ -1,4 +1,4 @@
-import { Button, Form } from "antd"
+import { Button, DatePicker, Form, Select } from "antd"
 import Input from "antd/es/input/Input"
 
 function FirstForm() {
@@ -24,6 +24,38 @@ function FirstForm() {
               ]}>
                   <Input placeholder="Email"/>
               </Form.Item>
+              <Form.Item label="Password" name="password" rules={[
+                  {
+                      required: true,
+                      message:"Please Enter Password",
+                  }
+              ]}>
+                  <Input></Input>
+              </Form.Item>
+              <Form.Item
+      label="Select"
+      name="Select"
+      rules={[
+        {
+          required: true,
+          message: 'Please input!',
+        },
+      ]}
+    >
+      <Select />
+              </Form.Item>
+              <Form.Item
+      label="DatePicker"
+      name="DatePicker"
+      rules={[
+        {
+          required: true,
+          message: 'Please input!',
+        },
+      ]}
+    >
+      <DatePicker />
+    </Form.Item>
               <Button type="primary" htmlType="submit">Submit</Button>
           </Form>
     </div>
