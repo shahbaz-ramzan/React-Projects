86df6766f0cd84256b48690774c83b4dfb39e17a
Shahbaz-ramzan-01 <mshahbazramzan952@gmail.com>
2024-03-27 22:00:54 +0500

    latest code

--- form-validations/src/components/password/password.jsx
+++ form-validations/src/components/password/password.jsx
@@ -1,134 +1,123 @@
 import { Button, Form, Input, Popover } from "antd";
-import PasswordValidation from "./passwordValidation";
-import { useForm } from "antd/es/form/Form";
+import PasswordValidation from "./passwordValidation"; // Assuming this component is correctly implemented
+import { useForm } from "antd";
 import { useState } from "react";
 
-
-
 function Password() {
+  const [password, setPassword] = useState('');
 
-    const form = useForm()
-    const [password, setPassword] = useState();
+  const form = useForm();
 
-
-    const resetPassword = async (values) => {
-        console.log("Validating password",values)
-      };
+  const resetPassword = async (values) => {
+    console.log("Validating password", values);
+  };
 
   return (
-      <>
+    <>
       <Form
-            form={form}
-            name="basic"
-            initialValues={{
-              remember: true,
-            }}
-            onFinish={resetPassword}
-            layout="vertical"
+        form={form}
+        name="basic"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={resetPassword}
+        layout="vertical"
+      >
+        <Form.Item
+          label="First Name"
+          name="firstName"
+          rules={[
+            {
+              required: true,
+              message: "Please input your name",
+            },
+            {
+              min: 3,
+              message: "First name must be minimum 3 characters.",
+            },
+            {
+              max: 14,
+              message: "First name should be less than 15 characters",
+            },
+          ]}
+        >
+          <Input placeholder="First Name" size="large" />
+        </Form.Item>
+        <Form.Item
+          label="Last Name"
+          name="lastName"
+          rules={[
+            {
+              required: true,
+              message: "Please input your last name",
+            },
+            {
+              min: 3,
+              message: "Last name must be minimum 3 characters.",
+            },
+            {
+              max: 14,
+              message: "Last name should be less than 15 characters",
+            },
+          ]}
+        >
+          <Input placeholder="Last Name" size="large" />
+        </Form.Item>
+        <Popover
+          content={<PasswordValidation password={password} />}
+          trigger="focus"
+          placement="leftTop"
+        >
+          <Form.Item
+            label="New Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input password",
+              },
+            ]}
           >
-            
-                <Form.Item
-                  label="First Name"
-                  name="firstName"
-                  rules={[
-                    {
-                      required: true,
-                      message: "Please input your name",
-                    },
-
-                    {
-                      min: 3,
-                      message: "First name must be minimum 3 characters.",
-                    },
-                    {
-                      max: 14,
-                      message: "First name should be less than 15 character",
-                    },
-                    
-                  ]}
-                >
-                  <Input placeholder="First Name" size="large" />
-                </Form.Item>
-                <Form.Item
-                  label="Last Name"
-                  name="lastName"
-                  rules={[
-                    {
-                      required: true,
-                      message: "please input your last name",
-                    },
-                    
-                    {
-                      min: 3,
-                      message: "Last name must be minimum 3 characters.",
-                    },
-                    {
-                      max: 14,
-                      message: "Last name should be less than 15 character",
-                    },
-                  ]}
-                >
-                  <Input placeholder="Last Name" size="large" />
-                </Form.Item>
-            <Popover
-              content={<PasswordValidation password={password} />}
-              trigger={"focus"}
-              placement="leftTop"
-            >
-              <Form.Item
-                label="New Password"
-                name="password"
-                rules={[
-                  {
-                    required: true,
-                    message: "please input password",
-                  },
-                  
-                ]}
-              >
-                <Input.Password
-                  suffix={<i className="icon-lock"></i>}
-                  placeholder="New Password"
-                  size="large"
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </Form.Item>
-            </Popover>
-            <Form.Item
-              label="Confirm New Password"
-              name="confirmPassword"
-              dependencies={["password"]}
-              rules={[
-                {
-                  required: true,
-                  message: "please confirm your password!",
-                },
-                ({ getFieldValue }) => ({
-                  validator(_, value) {
-                    if (!value || getFieldValue("password") === value) {
-                      return Promise.resolve();
-                    }
-                    return Promise.reject(
-                      new Error("Password does not match!")
-                    );
-                  },
-                }),
-              ]}
-            >
-              <Input.Password
-                suffix={<i className="icon-lock"></i>}
-                placeholder="Confirm Password"
-                size="large"
-              />
-            </Form.Item>
-            <Form.Item>
-              <Button block size="large" type="primary" htmlType="submit">
-                Update Password
-              </Button>
-            </Form.Item>
-        </Form >
-          </>
-  )
+            <Input.Password
+              placeholder="New Password"
+              size="large"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </Form.Item>
+        </Popover>
+        <Form.Item
+          label="Confirm New Password"
+          name="confirmPassword"
+          dependencies={["password"]}
+          rules={[
+            {
+              required: true,
+              message: "Please confirm your password!",
+            },
+            ({ getFieldValue }) => ({
+              validator(_, value) {
+                if (!value || getFieldValue("password") === value) {
+                  return Promise.resolve();
+                }
+                return Promise.reject(new Error("Password does not match!"));
+              },
+            }),
+          ]}
+        >
+          <Input.Password
+            placeholder="Confirm Password"
+            size="large"
+            htmlType="button" // Prevent form submission on Enter key press
+          />
+        </Form.Item>
+        <Form.Item>
+          <Button block size="large" type="primary" htmlType="submit">
+            Update Password
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
 }
 
-export default Password
+export default Password;
